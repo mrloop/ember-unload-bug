0eb0e2b82187f149728a43edcf5c8585e04e3160
mrloop <ewan.mcdougall.ext@qonto.com>
2022-11-03 11:26:00 +0000

    Minimal reproduction of incorrect peekAll length

---
+++ app/routes/application.js
@@ -0,0 +1,20 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import { assert } from '@ember/debug';
+
+export default class ApplicationRoute extends Route {
+  @service store;
+
+  model() {
+    assert(
+      'There should be 0 models before createRecord',
+      this.store.peekAll('my-model').length === 0
+    );
+    let model = this.store.createRecord('my-model');
+    model.unloadRecord();
+    assert(
+      'There should be 0 models after unloadRecord',
+      this.store.peekAll('my-model').length === 0
+    );
+  }
+}
